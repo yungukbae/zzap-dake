8a35cbfac9896b6fce6dd1d2b9168ac3c0e1e173
Capamax <maximum@capa.ai>
2023-11-17 14:07:45 +0900

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useRef, useState } from "react";
 
 function App() {
+  const [draw, setDraw] = useState(false);
+  const [pos, setPos] = useState({ drawable: false, x: -1, y: -1 });
+  const isRef = useRef<HTMLCanvasElement>(null);
+
+  useEffect(() => {
+    const img1 = new Image();
+    const img2 = new Image();
+    const ctx = isRef.current?.getContext("2d");
+    if (!ctx) return () => {};
+
+    img1.src = "https://capa.ai/static/services/one_stop.jpg";
+    img2.src = "https://capa.ai/static/services/cutting.jpg";
+
+    const pattern1 = ctx.createPattern(img1, "no-repeat");
+    const pattern2 = ctx.createPattern(img2, "no-repeat");
+
+    ctx.lineWidth = 80;
+    ctx.lineCap = "round";
+    ctx.lineJoin = "round";
+    ctx.strokeStyle = pattern2 as CanvasPattern;
+    if (!draw) {
+      ctx.fillStyle = pattern1 as CanvasPattern;
+      ctx.fillRect(0, 0, 1100, 1100);
+      setDraw(true);
+    }
+
+    const handleUp = () => {
+      setPos((va) => ({ ...va, drawable: false }));
+    };
+
+    const handleDown = (e: MouseEvent) => {
+      const target = e as MouseEvent;
+      ctx.beginPath();
+      ctx.moveTo(target.offsetX, target.offsetY);
+      setPos({ drawable: true, x: target.offsetX, y: target.offsetY });
+    };
+
+    const handleMove = (e: MouseEvent) => {
+      const target = e as MouseEvent;
+      if (pos.drawable) {
+        ctx.lineTo(target.offsetX, target.offsetY);
+        ctx.stroke();
+      }
+    };
+
+    window.addEventListener("mousedown", handleDown);
+    window.addEventListener("mouseup", handleUp);
+    window.addEventListener("mousemove", handleMove);
+    return () => {
+      window.removeEventListener("mousedown", handleDown);
+      window.removeEventListener("mouseup", handleUp);
+      window.removeEventListener("mousemove", handleMove);
+    };
+  }, [isRef, pos]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{ width: "100%", height: "100%" }}>
+        <canvas ref={isRef} width={"1100px"} height={"1100px"}></canvas>
+      </div>
     </div>
   );
 }
